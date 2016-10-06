6cf3b5c5c76f197282b06e831e90e01c5610b82c
Almaz <diamond.scalping@yandex.com>
2016-10-06 10:45:34 +0500

    v1.1.1

--- class.search_parse.php
+++ class.search_parse.php
@@ -1,10 +1,28 @@
 <?php
+/*
+  Copyright (C) 2016 DevDiamond. (email : me@devdiamond.com)
+
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU General Public License
+  as published by the Free Software Foundation; either version 2
+  of the License, or (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
 /**
- * class WP_SearchParse, to optimize | searching | filtering of search words
+ * class WP_SearchParse - to optimize | searching | filtering of search words
  *
  * @link    https://github.com/DevDiamondCom/WP_SearchParse
- * @version 1.1.0
+ * @version 1.1.1
  * @author  DevDiamond <me@devdiamond.com>
+ * @license GPLv2 or later
  */
 class WP_SearchParse
 {
